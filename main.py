f97421965b718d971a710e4e16a68b4778cd49cd
mac01 <mac01@gmail.com>
2024-07-02 14:23:42 +0800

    1

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import websockets
+
+# 存储所有连接的客户端
+clients = {}
+
+async def handle_client(websocket, path):
+    name = await websocket.recv()
+    clients[websocket] = name
+    try:
+        async for message in websocket:
+            # 解析消息格式为 "to:recipient,message:content"
+            recipient, content = message.split(",", 1)
+            for client, client_name in clients.items():
+                if client_name == recipient:
+                    await client.send(content)
+    finally:
+        del clients[websocket]
+
+start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
