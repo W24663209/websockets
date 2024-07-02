fb1797395cb827efc6dccf6cda9f2c3df3eaddec
mac01 <mac01@gmail.com>
2024-07-02 15:09:29 +0800

    1

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import asyncio
 import websockets
+import logging
+
+# 配置日志输出格式
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # 存储所有连接的客户端
 clients = {}
-print('开始启动')
+logging.info('开始启动')
 async def handle_client(websocket, path):
     name = await websocket.recv()
-    print(name)
+    logging.info(name)
     clients[websocket] = name
     try:
         async for message in websocket:
             # 解析消息格式为 "to:recipient,message:content"
             recipient, content = message.split(",", 1)
-            print(message)
+            logging.info(message)
             for client, client_name in clients.items():
                 if client_name == recipient:
                     await client.send(content)
@@ -20,6 +24,6 @@ async def handle_client(websocket, path):
         del clients[websocket]
 
 start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
-print('启动成功')
+logging.info('启动成功')
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
