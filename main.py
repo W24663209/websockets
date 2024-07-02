54459c9db7b8c242e63a8acfc6c1e17bab5248b6
mac01 <mac01@gmail.com>
2024-07-02 14:58:33 +0800

    1

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ clients = {}
 
 async def handle_client(websocket, path):
     name = await websocket.recv()
+    print(name)
     clients[websocket] = name
     try:
         async for message in websocket:
             # 解析消息格式为 "to:recipient,message:content"
             recipient, content = message.split(",", 1)
+            print(message)
             for client, client_name in clients.items():
                 if client_name == recipient:
                     await client.send(content)
