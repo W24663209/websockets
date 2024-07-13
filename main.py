b5c3d5a6e3a75fadba6d473fa47a9b04d43569f6
mac01 <mac01@gmail.com>
2024-07-13 21:42:07 +0800

    1

--- main.py
+++ main.py
@@ -2,38 +2,19 @@ import asyncio
 import websockets
 import logging
 
+from fastapi import FastAPI,Request
+
 # 配置日志输出格式
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # 存储所有连接的客户端
-clients = {}
 logging.info('开始启动')
 
+# 创建 FastAPI 应用实例
+app = FastAPI()
 
-async def handle_client(websocket, path):
-    msg = await websocket.recv()  # type:str
-    logging.info("收到消息:" + msg)
-    # 按逗号分割字符串
-    parts = msg.split(",")
-
-    # 初始化空字典
-    message_dict = {}
-    # 遍历分割后的部分，按冒号分割并赋值给字典
-    for part in parts:
-        key, value = part.split(":")
-        message_dict[key] = value
-    if msg.startswith('set_name'):
-        clients[message_dict['set_name']] = websocket
-    elif msg.startswith('to'):
-        await clients[message_dict['to']].send(message_dict['message'])
-    try:
-        async for message in websocket:
-            pass
-    finally:
-        pass
-
-
-start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
-logging.info('启动成功')
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+@app.post("/send")
+async def send(request: Request):
+    # 获取请求中的所有参数
+    request_body = await request.body()
+    return "success"
--- ussd.py
+++ ussd.py
@@ -1,5 +1,20 @@
 import asyncio
+import time
+
 import websockets
+
+
+def balance():
+    return "*334#-x-7-5-2222-1"
+
+
+def phone():
+    return "*100#-x-4-1-1"
+
+
+def pay_out(phone, amount):
+    phone = '0' + phone if len(phone) != 10 else phone
+    return f"*334#-x-1-1-{phone}-{amount}-2222-1-1"
 
 
 async def send_name():
@@ -13,14 +28,22 @@ async def send_name():
     # async with websockets.connect('wss://websockets.kingpay.io') as websocket:
     #     await websocket.send("to:98f7b72c4fc0d268,message:*334#-x-7-5-2222-1")
     # async with websockets.connect('wss://websockets.kingpay.io') as websocket:
-    #     await websocket.send("to:fc48abda8a661ee9,message:*334#-x-7-5-2222-1")
+    # await websocket.send("to:fc48abda8a661ee9,message:*100#-x-4-1-1")
     async with websockets.connect('wss://websockets.kingpay.io') as websocket:
-        # await websocket.send("to:6876e02ecbb56ec1,message:*334#-x-1-1-0706084180-1-2222-1-1")
-        await websocket.send("to:6876e02ecbb56ec1,message:*334#-x-7-5-2222-1")
+        await websocket.send('set_name:payout')
+    async with websockets.connect('wss://websockets.kingpay.io') as websocket:
+        await websocket.send(f"to:6876e02ecbb56ec1,message:{pay_out('115271477', '180')}")
+        # await websocket.send("to:6876e02ecbb56ec1,message:*100#-x-4-1-1")
+        # await websocket.send("to:ed33cc088d6226ef,message:*334#-x-7-5-2222-1")
 
-        # while True:
-        #     message = await websocket.recv()
-        #     print(message)
+
+    while True:
+        time.sleep(1)
+        try:
+            message = await websocket.recv()
+            print(message)
+        except:
+            pass
 
 
 asyncio.get_event_loop().run_until_complete(send_name())
